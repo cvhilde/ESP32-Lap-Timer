192be158e0caefd76a22d64cdd5ad0d63b1867e8
cvhilde <cvhilde1@gmail.com>
2025-05-23 17:41:32 -0400

    sector timing logic done. updated screen display logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,24 @@
 #include <Wire.h>
 #include "HT_SSD1306Wire.h"
-#include <TinyGPS++.h>
 #include <HardwareSerial.h>
 #include <math.h>
+#include <SPIFFS.h>
+#include <FS.h>
+#include <time.h>
+#include <TimeLib.h>
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
+#include <display.h>
 
 // #define gpsRX = 20
 // #define gpsTX = 19
 
 static SSD1306Wire display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED);
-TinyGPSPlus gps;
+SFE_UBLOX_GNSS gps;
 HardwareSerial GPS(1);
-TinyGPSCustom satsInView(gps, "GPGSV", 3);
-
-TinyGPSCustom satNumber[4];
 
 #define VBAT_Read 1
 #define ADC_CTRL 37
-uint16_t samples[500];
+uint16_t samples[20];
 int avgIndex = 0;
 
 typedef struct coord {
@@ -37,6 +39,26 @@ int currSector = 0;
 const float EARTH_RADIUS_FT = 20902230.0f;
 const float DEG_TO_RADIANS = 0.017453292519943295f;
 
+#define BUTTON_PIN 0
+bool sessionActive = false;
+String currentTimestamp = "";
+
+static unsigned long loopTime = 0; 
+
+static unsigned long lastUpdate = 0;
+static unsigned long lastCrossTime = 0;
+static unsigned long lastSectorTime = 0;
+unsigned long lastLapTime = 0;
+static unsigned long lastButtonPress = 0;
+static unsigned long lastDraw = 0;
+static bool prevButtonState = HIGH;
+
+static unsigned long previousLapTime = 0;
+static unsigned long sector1Time = 0;
+static unsigned long sector2Time = 0;
+static unsigned long sector3Time = 0;
+static bool firstLap = 0;
+
 float readBattVoltage();
 uint32_t getSatCount();
 double getLongitude();
@@ -48,6 +70,9 @@ int orientation(coord p1, coord p2, coord p3);
 bool doIntersect(coord prev, coord curr, wayPoint wp);
 void storeCurrLocation(double lat, double lng);
 coord midPoint(wayPoint wp);
+void syncTimeWithGPS();
+void startSession();
+void drawLaptime(unsigned long time);
 
 
 void VextON() {
@@ -98,28 +123,56 @@ void drawDistance(double distance) {
     display.display();
 }
 
-void drawLaptime(double laptime) {
+void drawLaptime(unsigned long lapTime, unsigned long sector1, unsigned long sector2, unsigned long sector3, unsigned long lastLap) {
+    unsigned int min = lapTime/60000;
+    unsigned int sec = (lapTime % 60000) / 1000;
+    unsigned int tenths = (lapTime % 1000) / 100;
+
+    unsigned int min1 = sector1/60000;
+    unsigned int sec1 = (sector1 % 60000) / 1000;
+    unsigned int tenths1 = (sector1 % 1000) / 100;
+
+    unsigned int min2 = sector2/60000;
+    unsigned int sec2 = (sector2 % 60000) / 1000;
+    unsigned int tenths2 = (sector2 % 1000) / 100;
+
+    unsigned int min3 = sector3/60000;
+    unsigned int sec3 = (sector3 % 60000) / 1000;
+    unsigned int tenths3 = (sector3 % 1000) / 100;
+
+    unsigned int min4 = lastLap/60000;
+    unsigned int sec4 = (lastLap % 60000) / 1000;
+    unsigned int tenths4 = (lastLap % 1000) / 100;
+
+    char curr[30];
+    char sect1[30];
+    char sect2[30];
+    char sect3[30];
+    char last[30];
+    sprintf(curr, "%u:%02u.%u", min, sec, tenths);
+    sprintf(sect1, "S1 %u:%02u.%u", min1, sec1, tenths1);
+    sprintf(sect2, "S2 %u:%02u.%u", min2, sec2, tenths2);
+    sprintf(sect3, "S3 %u:%02u.%u", min3, sec3, tenths3);
+    sprintf(last, "L %u:%02u.%u", min4, sec4, tenths4);
     display.clear();
-    char str[20];
-    display.setTextAlignment(TEXT_ALIGN_CENTER);
-    display.drawString(display.width()/2, 0, "Passed waypoint " + String(currSector));
-    sprintf(str, "%.2f seconds", laptime);
-    display.drawString(display.width()/2, 20, str);
+    display.drawString(40, 5, curr);
+    display.drawString(0, 32, sect1);
+    display.drawString(64, 32, sect2);
+    display.drawString(0, 50, sect3);
+    display.drawString(64, 50, last);
     display.display();
-    delay(5000);
 }
 
 double getLatitude() {
-    return gps.location.isValid() ? gps.location.lat() : 0.0;
+    return gps.getLatitude() / 10000000.0;
 }
 
 double getLongitude() {
-    return gps.location.isValid() ? gps.location.lng() : 0.0;
+    return gps.getLongitude() / 10000000.0;
 }
 
 uint32_t getSatCount() {
-    int visible = atoi(satsInView.value());
-    return visible;
+    return gps.getSIV();
 }
 
 float readBattVoltage() {
@@ -127,14 +180,14 @@ float readBattVoltage() {
     samples[avgIndex++] = analogRead(VBAT_Read);
     digitalWrite(ADC_CTRL, LOW);
 
-    if (avgIndex >= 500) {
+    if (avgIndex >= 20) {
         avgIndex = 0;
     }
     uint32_t total = 0;
-    for (int i = 0; i < 500; i++) {
+    for (int i = 0; i < 20; i++) {
         total += samples[i];
     }
-    float avgRaw = total / 500.0;
+    float avgRaw = total / 20.0;
     return avgRaw * (3.3 / 4095.0) * 5.215384 * 1.073655914; // Just 5.215384 for 2nd board
 }
 
@@ -194,8 +247,47 @@ coord midPoint(wayPoint wp) {
     coord mid;
     mid.lat = (wp.p1.lat + wp.p2.lat) / 2.0;
     mid.lng = (wp.p1.lng + wp.p2.lng) / 2.0;
-    Serial.printf("%.7f lat | %.7f lng\n", mid.lat, mid.lng);
     return mid;
+}
+
+void startSession() {
+    uint16_t year = gps.getYear();
+    uint8_t month = gps.getMonth();
+    uint8_t day = gps.getDay();
+    uint8_t hour = gps.getHour() - 4;
+    uint8_t minute = gps.getMinute();
+    uint8_t second = gps.getSecond();
+
+    if (hour < 0) {
+        hour += 24;
+    }
+
+    char timestamp[50];
+    sprintf(timestamp, "%04d%02d%02d_%02d%02d", year, month, day, hour, minute);
+    currentTimestamp = String(timestamp);
+
+    String logFile = "/log_" + currentTimestamp + ".csv";
+    String timeFile = "/timestamps_" + currentTimestamp + ".csv";
+
+    Serial.printf("GNSS NAV-PVT time: %04d-%02d-%02d %02d:%02d:%02d\n", year, month, day, hour, minute, second);
+
+    File log = SPIFFS.open(logFile, FILE_WRITE);
+    File time = SPIFFS.open(timeFile, FILE_WRITE);
+    if (!log || !time) {
+        Serial.println("Failed to create session files");
+    }
+
+    log.close();
+    time.close();
+
+    File manifest = SPIFFS.open("/sessions.txt", FILE_APPEND);
+    if (manifest) {
+        manifest.println(currentTimestamp);
+        manifest.close();
+    }
+
+    Serial.println("Sessions started: " + currentTimestamp);
+    sessionActive = true;
 }
 
 
@@ -211,13 +303,13 @@ void setup() {
 
     GPS.begin(115200, SERIAL_8N1, 19, 20);
     delay(1000);
-
-    for (int i = 0; i < 4; ++i) {
-        satNumber[i].begin(gps, "GPGSV", 4 + 4*i);
+    if (!gps.begin(GPS)) {
+        Serial.println("GNSS init failed");
+        while (1);
     }
 
     display.init();
-    display.setFont(ArialMT_Plain_10);
+    display.setFont(Roboto_Light_14);
 
     for (int i = 0; i < 2; i++) {
         activeLocations[i].lat = 0;
@@ -241,25 +333,38 @@ void setup() {
     trackWaypoints[2].p2.lat = 28.61365188;
     trackWaypoints[2].p2.lng = -81.17954403;
     trackWaypoints[2].isActive = 1;
+
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    if (!SPIFFS.begin(true)) {
+        Serial.println("SPIFFS mount failed");
+        return;
+    }
 }
 
 void loop() {
-    while (GPS.available()) {
-        gps.encode(GPS.read());
-    }
-
-    static unsigned long lastUpdate = 0;
-    static unsigned long lastCrossTime = 0;
-    static unsigned long lastSectorTime = 0;
-    static unsigned long lastLapTime = 0;
+    gps.checkUblox();
     if (millis() - lastUpdate >= 100) {
         lastUpdate = millis();
-        if (gps.location.isValid()) {
+        if (gps.getFixType() > 1) {
+            bool currButtonState = digitalRead(BUTTON_PIN);
+            if (prevButtonState == HIGH && currButtonState == LOW && millis() - lastButtonPress > 1000 && sessionActive == false) {
+                lastButtonPress = millis();
+                startSession();
+            } else if (prevButtonState == HIGH && currButtonState == LOW && millis() - lastButtonPress > 1000 && sessionActive == true) {
+                sessionActive = false;
+                //endSession() logic;
+                Serial.println("Ending session");
+            }
+            prevButtonState = currButtonState;
+
             double lat = getLatitude();
             double lng = getLongitude();
             uint32_t sats = getSatCount();
-            float vcc = readBattVoltage();
             storeCurrLocation(lat, lng);
+
+            if (sessionActive == true) {
+                // log data
+            }
 
             double distance = distanceCalc(activeLocations[0], trackWaypoints[currSector]);
             if (trackWaypoints[currSector].isActive) {
@@ -267,12 +372,25 @@ void loop() {
                     if (millis() - lastCrossTime > 15000) {
                         lastCrossTime = millis();
                         if (currSector == 0) {
-                            drawLaptime((millis() - lastLapTime) / 1000.0);
-                            lastLapTime = millis();
-                            lastSectorTime = millis();
+                            if (firstLap == 0) {
+                                lastLapTime = millis();
+                                lastSectorTime = millis();
+                                firstLap = 1;
+                            } else if (firstLap == 1) {
+                                previousLapTime = millis() - lastLapTime;
+                                lastLapTime = millis();
+                                sector3Time = millis() - lastSectorTime;
+                                lastSectorTime = millis();
+                            }
+                            // log last sector and lap time
                         } else {
-                            drawLaptime((millis() - lastSectorTime) / 1000.0);
-                            lastSectorTime = millis();
+                            if (currSector == 1) {
+                                sector1Time = millis() - lastSectorTime;
+                                lastSectorTime = millis();
+                            } else if (currSector == 2) {
+                                sector2Time = millis() - lastSectorTime;
+                                lastSectorTime = millis();
+                            }
                         }
                         currSector++;
                         if (currSector > 2) {
@@ -282,8 +400,12 @@ void loop() {
                 }
             }
 
-            Serial.printf("lat: %.5lf | long: %.5lf | sats: %d | dist %.2f\n\n", lat, lng, sats, distance);
-            drawScreen(lat, lng, distance);
+            //Serial.printf("lat: %.5lf | long: %.5lf | sats: %d | dist %.2f\n\n", lat, lng, sats, distance);
+            //if (millis() - lastDraw >= 500) {
+            drawLaptime(millis() - lastLapTime, sector1Time, sector2Time, sector3Time, previousLapTime);
+                //lastDraw = millis();
+            //}
+            
         } else {
             uint32_t sats = getSatCount();
             Serial.printf("sats: %d\n", sats);
@@ -296,4 +418,7 @@ void loop() {
             display.display();
         }
     }
+    Serial.println(millis() - loopTime);
+    loopTime = millis();
+
 }
